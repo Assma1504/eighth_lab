b98f89b47d6737825e7f8529e3a8708e85fcde5f
Assma1504 <assmabaziz15@gamil.com>
2026-03-18 19:10:26 +0300

    first two tasks done

---
+++ first_task.py
@@ -0,0 +1,19 @@
+myDict ={
+    "Algeria": "Algeria",
+    "Russia": "Moscow",
+    "Latvia": "Riga",
+    "France": "Paris",
+    "Germany": "Berlin",
+    "Italy": "Rome",
+    "Spain": "Madrid",
+    "United Kingdom": "London",
+    "Canada": "Ottawa",
+    "Japan": "Tokyo",
+    "Australia": "Canberra"
+}
+
+print(myDict)
+# for key, value in myDict.items():
+#     print(f"{key} : {value}")
+print(myDict["Algeria"])
+print(sorted(myDict))
---
+++ second_task.py
@@ -0,0 +1,51 @@
+myDict = {
+    "onePoint":["А", "В", "Е", "И", "Н", "О", "Р", "С", 'Т' ],
+    "twoPoints:" :["Д", "К", "Л", "М", "П", "У" ],
+    "threePoints:" :["Б", "Г", "Ё", "Ь", "Я"],
+    "fourPoints:" :["Ы ", "Й"],
+    "fivePoints:" :["Ж", "З", "Х", "Ц", "Ч "],
+    "eightPoints:" :["Ш", "Э", "Ю"],
+    "tenPoints:" :["Ф", "Щ", "Ъ"],
+}
+
+userValue = input("let's start playing, enter a word, to count the score: ")
+sumPoints = 0
+
+for item in userValue:
+
+    match item:
+        case one if item in myDict["onePoint"]:
+            sumPoints += 1
+        case two if item in myDict["twoPoints:"]:
+            sumPoints += 2
+        case three if item in myDict["threePoints:"]:
+            sumPoints += 3
+        case four if item in myDict["fourPoints:"]:
+            sumPoints += 4
+        case five if item in myDict["fivePoints:"]:
+            sumPoints +=5
+        case eight if item in myDict["eightPoints:"]:
+            sumPoints += 8
+        case ten if item in myDict["tenPoints:"]:
+            sumPoints += 10
+        case _:
+            sumPoints = 0
+
+    # if item in myDict["onePoint"]: 
+    #     sumPoints += 1
+    #     print("one point")
+    # elif item in myDict["twoPoints:"]:
+    #     sumPoints +=2
+    #     print("2 point")
+    # elif item in myDict["fivePoints:"]:
+    #     sumPoints +=5
+    #     print("5 point")
+
+print(f"The Total score is: {sumPoints}")
+
+if sumPoints == 0: 
+    print("May be you enterd some wrong characters")
+
+
+
+
